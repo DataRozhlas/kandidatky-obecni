e9a97eb512fe37ae6045aaf2c0450804d63e685c
Petr Kočí <petr.koci@samizdat.cz>
2022-08-12 17:58:03 +0200

    deploy test

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>Ahoj!</h1>
+        <h1 className={styles.title}>Ahoj!!!!</h1>
       </main>
     </div>
   );
