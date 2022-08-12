6047c09fc681cbc978fe81364a2be7b5d32d96dd
Petr Kočí <petr.koci@samizdat.cz>
2022-08-12 16:28:35 +0200

    deploy test

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  // images: {
+  //   loader: "custom",
+  // },
+  trailingSlash: true,
+  //  basePath: "/",
+  experimental: {
+    images: {
+      unoptimized: true,
+    },
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
---
+++ utils/imgLoader.js
@@ -0,0 +1,5 @@
+const imgLoader = ({ src, width, quality }) => {
+  return `https://data.irozhlas.cz/kandidatky-obecni${src}`;
+};
+
+export default imgLoader;
