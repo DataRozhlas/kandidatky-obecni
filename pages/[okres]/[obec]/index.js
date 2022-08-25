02bc66c8922054179a4e0a9eb88bda5c2f29da22
Petr Kočí <petr.koci@samizdat.cz>
2022-08-25 12:41:44 +0200

    load candidates dynamically

--- pages/[okres]/[obec]/index.js
+++ pages/[okres]/[obec]/index.js
@@ -1,8 +1,12 @@
 import { tsvParse } from "d3-dsv";
+import { QueryClient, QueryClientProvider, useQuery } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 
 import okresy from "../../../public/okresy.json";
 
 import styles from "../../../styles/Obec.module.css";
+
+const queryClient = new QueryClient();
 
 export async function getStaticProps({ params }) {
   //info o obci
@@ -13,16 +17,18 @@ export async function getStaticProps({ params }) {
     .then(res => tsvParse(res));
   const obecData = okresZastupitelstva.find(obec => obec.key === params.obec);
   //seznam kandidátů
-  const obecKandidati = await fetch(
-    `https://data.irozhlas.cz/kandidatky-obecni-data/2022/${params.okres}/${params.obec}/kandidati.tsv`
-  )
-    .then(res => res.text())
-    .then(res => tsvParse(res));
+  // const obecKandidati = await fetch(
+  //   `https://data.irozhlas.cz/kandidatky-obecni-data/2022/${params.okres}/${params.obec}/kandidati.tsv`
+  // )
+  //   .then(res => res.text())
+  //   .then(res => tsvParse(res));
+  const okresData = okresy.find(okres => okres.NUMNUTS === obecData.OKRES);
 
   return {
     props: {
       obecData,
-      obecKandidati,
+      okresData,
+      // obecKandidati,
     },
   };
 }
@@ -42,11 +48,33 @@ export async function getStaticPaths() {
   };
 }
 
-const Obec = ({ obecData, obecKandidati }) => {
+const ObecPage = ({ obecData, okresData }) => {
+  const { isLoading, error, data } = useQuery("candidatesData", () =>
+    fetch(
+      `https://data.irozhlas.cz/kandidatky-obecni-data/2022/${okresData.key}/${obecData.key}/kandidati.tsv`
+    )
+      .then(res => res.text())
+      .then(res => tsvParse(res))
+  );
+
+  if (isLoading) return "Stahuji data...";
+
+  if (error) return "Stala se chyba: " + error.message;
+
   return (
     <>
       <h1>{obecData.NAZEVZAST}</h1>
+      <h2>{data.length} kandidátů</h2>
     </>
+  );
+};
+
+const Obec = ({ obecData, okresData }) => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ObecPage obecData={obecData} okresData={okresData} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
 
