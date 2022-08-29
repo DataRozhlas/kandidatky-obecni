b3c8528cb157d83d4223de2713862c1a7e078289
Petr Kočí <petr.koci@samizdat.cz>
2022-08-29 14:06:18 +0200

    mobile viewport

--- pages/_app.js
+++ pages/_app.js
@@ -20,7 +20,10 @@ function MyApp(props) {
   return (
     <CacheProvider value={emotionCache}>
       <Head>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0"
+        />
       </Head>
       <ThemeProvider theme={theme}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
